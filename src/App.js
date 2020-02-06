ea10706eb6b5adba56a344a25012c5d9d047a6f1
Daniel Simonson (contractor) <BH15491@federated.fds>
2020-02-06 14:45:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>file download</h1>
+      <form>
+        <textarea>Download me as file data.</textarea>
+        <a href="javascript:void(0)" download="data.txt">
+          Download Text
         </a>
-      </header>
+      </form>
     </div>
   );
 }
